532e21d3c8214ddc9b24cba0306770348a636d74
mahesh <maheshkumar@gmail.com>
2023-08-11 16:00:01 +0530

    improve error handler

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,85 +1,66 @@
 import Product from "../models/productModel.js";
 import ErrorHandler from "../utils/errorHandler.js";
+import catchAsyncError from "../middlewares/catchAsyncError.js";
 
-export const getAllProducts = async (req, res, next) => {
-  try {
-    const products = await Product.find();
-    if (!products) {
-      return next(new ErrorHandler("Product not found", 404));
-    }
-    return res.status(200).json({
-      success: true,
-      products,
-    });
-  } catch (err) {
-    return next(new ErrorHandler(err.message, err.statusCode));
+export const getAllProducts = catchAsyncError(async (req, res, next) => {
+  const products = await Product.find();
+  if (!products) {
+    return next(new ErrorHandler("Product not found", 404));
   }
-};
+  return res.status(200).json({
+    success: true,
+    products,
+  });
+});
 
-export const getSingleProduct = async (req, res, next) => {
-  try {
-    const product = await Product.findById(req.params.id);
-    if (!product) {
-      return next(new ErrorHandler("Product not found", 404));
-    }
-    return res.status(200).json({
-      success: true,
-      product,
-    });
-  } catch (err) {
-    return next(new ErrorHandler(err.message, err.statusCode));
+export const getSingleProduct = catchAsyncError(async (req, res, next) => {
+  const product = await Product.findById(req.params.id);
+  if (!product) {
+    return next(new ErrorHandler("Product not found", 404));
   }
-};
+  return res.status(200).json({
+    success: true,
+    product,
+  });
+});
 
-export const addProduct = async (req, res, next) => {
-  try {
-    const product = await Product.create(req.body);
-    if (!product) {
-      return next(new ErrorHandler("Product not found", 404));
-    }
-    return res.status(200).json({
-      success: true,
-      product,
-    });
-  } catch (err) {
-    return next(new ErrorHandler(err.message, err.statusCode));
+export const addProduct = catchAsyncError(async (req, res, next) => {
+  const product = await Product.create(req.body);
+  if (!product) {
+    return next(new ErrorHandler("Product not found", 404));
   }
-};
+  return res.status(200).json({
+    success: true,
+    product,
+  });
+});
 
-export const deleteProduct = async (req, res, next) => {
-  try {
-    const product = await Product.findById(req.params.id);
-    if (!product) {
-      return next(new ErrorHandler("Product not found", 404));
-    }
-    await Product.findByIdAndDelete(req.params.id);
-    return res.status(200).json({
-      success: true,
-      message: "Product deleted successfully",
-    });
-  } catch (error) {
-    return next(new ErrorHandler(err.message, err.statusCode));
+export const deleteProduct = catchAsyncError(async (req, res, next) => {
+  const product = await Product.findById(req.params.id);
+  if (!product) {
+    return next(new ErrorHandler("Product not found", 404));
   }
-};
+  await Product.findByIdAndDelete(req.params.id);
+  return res.status(200).json({
+    success: true,
+    message: "Product deleted successfully",
+  });
+});
 
-export const updateProduct = async (req, res, next) => {
-  try {
-    const product = await Product.findById(req.params.id);
-    if (!product) {
-      return next(new ErrorHandler("Product not found", 404));
-    }
-    const updatedProduct = await Product.findByIdAndUpdate(
-      req.params.id,
-      req.body,
-      {
-        isModified: true,
-      }
-    );
-    return res.status(200).json({
-      success: true,
-      updatedProduct,
-    });
-  } catch (error) {
-    return next(new ErrorHandler(err.message, err.statusCode));
+export const updateProduct = catchAsyncError(async (req, res, next) => {
+  const product = await Product.findById(req.params.id);
+  if (!product) {
+    return next(new ErrorHandler("Product not found", 404));
   }
-};
+  const updatedProduct = await Product.findByIdAndUpdate(
+    req.params.id,
+    req.body,
+    {
+      isModified: true,
+    }
+  );
+  return res.status(200).json({
+    success: true,
+    updatedProduct,
+  });
+});
--- middlewares/error.js
+++ middlewares/error.js
@@ -2,10 +2,11 @@ import ErrorHandler from "../utils/errorHandler.js";
 
 export default (err, req, res, next) => {
   console.log("💥💥💥💥💥💥💥💥");
+
   err.statusCode = err.statusCode || 500;
   err.message = err.message || "Internal server error";
 
-  res.status(err.statusCode).json({
+  return res.status(err.statusCode).json({
     sucess: false,
     message: err.message,
   });
